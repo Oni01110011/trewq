a091524adbcef6a96ed1fad22e7fa04e8444200c
Oni <105129361+Oni01110011@users.noreply.github.com>
2026-01-06 14:17:44 +0100

    Add simple Java test code
    Co-Authored-By: Dante3-3 <dani.stoilovski9@gmail.com>

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Dies ist ein einfacher Java-Test.");
+    }
+    
+    public static int add(int a, int b) {
+        return a + b;
+    }
+    
+    public static boolean isEven(int number) {
+        return number % 2 == 0;
+    }
+}
